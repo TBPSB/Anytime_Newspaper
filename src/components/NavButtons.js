b0bf43944ba5153271412d1f11e9f151547a399b
Bhanu Pratap Singh Bisht <52692924+TBPSB@users.noreply.github.com>
2022-06-05 23:21:23 +0530

    Create NavButtons.js

---
+++ src/components/NavButtons.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Fab from "@material-ui/core/Fab";
+import NavigationIcon from "@material-ui/icons/Navigation";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: "#fff",
+    },
+}));
+
+const NavButtons = (props) => {
+    const classes = useStyles();
+
+    const prevPageHandler = () => {
+        props.change(-1);
+    };
+
+    const nextPageHandler = () => {
+        props.change(1);
+    };
+
+    const styles = {
+        buttonContainer: {
+            display: 'flex',
+            justifyContent: 'space-between',
+            margin: '10px 100px'
+        }
+    };
+
+    return (
+        <>
+            <Backdrop className={classes.backdrop} open={props.loading}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
+
+            <div style={styles.buttonContainer}>
+                <Fab
+                    size="small"
+                    color="secondary"
+                    area-label="Next"
+                    onClick={prevPageHandler}
+                >
+                    <NavigationIcon style={{ transform: "rotate(-90deg)" }} />
+                </Fab>
+                <Fab
+                    size="small"
+                    color="primary"
+                    area-label="Next"
+                    onClick={nextPageHandler}
+                >
+                    <NavigationIcon style={{ transform: "rotate(90deg)" }} />
+                </Fab>
+            </div>
+        </>
+    );
+};
+
+export default NavButtons;
